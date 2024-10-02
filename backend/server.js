0ccde96a870aaa75be78717fadc08580ea23fa36
osterava <olli.s.terava@student.jyu.fi>
2024-10-02 15:52:44 +0300

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('alutus');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
