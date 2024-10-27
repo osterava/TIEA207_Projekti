ea64ffa23d1dfa449cbca953af6df75cf572fafc
osterava <olli.s.terava@student.jyu.fi>
2024-10-27 11:14:32 +0200

    suomen data esillä

--- frontend/src/components/dataComponent.js
+++ frontend/src/components/dataComponent.js
@@ -1,17 +1,17 @@
     // Muunna data objektille taulukoksi
-    import React, { useEffect, useState } from 'react';
-    import { getData } from '../services/dataService'; // Muokkaa polku oikeaksi
+    import React, { useEffect, useState } from 'react'
+    import { getData } from '../services/dataService'
     
     const DataComponent = () => {
-        const [data, setData] = useState({});
-        const [loading, setLoading] = useState(true);
-        const [error, setError] = useState(null);
+        const [data, setData] = useState({})
+        const [loading, setLoading] = useState(true)
+        const [error, setError] = useState(null)
     
         useEffect(() => {
             const fetchData = async () => {
                 try {
                     const result = await getData()
-                    setData(result.values.GGXWDG_NGDP.SDN)
+                    setData(result.values.GGXWDG_NGDP.FIN)
                 } catch (err) {
                     setError(err.message)
                 } finally {
@@ -30,7 +30,7 @@
         return (
             <div>
                 <h1>Data List</h1>
-                <h2>Hakee vain tällä hetkellä ensimmäisen maan datan</h2>
+                <h2>Hakee vain tällä hetkellä Suomen datan</h2>
                 <h2> Tämä nyt vain hetkellisesti visualisoimassa, että se data oikeasti tulee fronttiin</h2>
                 <ul>
                     {dataEntries.map(([year, value]) => ( 
