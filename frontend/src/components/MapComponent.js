052dffebe74d50261ed24d611780c66a5d4c8b6d
pejuhukk <pejuhukk@student.jyu.fi>
2024-10-29 19:56:32 +0200

    aloitettiin maiden highlight toteutus. vielä hyvin raakile. toimii klikkaamalla ja lopettaa siirtämällä hiirtä pois pientä testailua varten.

--- frontend/src/components/MapComponent.js
+++ frontend/src/components/MapComponent.js
@@ -2,6 +2,64 @@ import React, { useEffect, useRef, useState } from 'react'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import mapService from '../services/mapService.js'
+import countries from '../data/countries.json'
+
+var geojson;
+
+/**
+ * Highlight-funktio. Tyyli placeholder
+ * @param {*} e 
+ */
+function highlightFeature(e) {
+  var layer = e.target;
+
+  layer.setStyle({
+      weight: 5,
+      // color: 'black',
+      fillColor: 'red',
+      // dashArray: '',
+      fillOpacity: 1
+  });
+
+  layer.bringToFront();
+}
+
+/**
+ * Resetoidaan highlight
+ * @param {*} e 
+ */
+function resetHighlight(e) {
+  geojson.resetStyle(e.target);
+}
+
+/**
+ * Laitetaan highlightit mouseoverilla placeholderina. Voidaan vaihtaa esim click
+ * @param {*} layer 
+ */
+function onEachFeature(feature, layer) {
+  layer.on({
+      // mouseover: highlightFeature,
+      // mouseout: resetHighlight
+      click: highlightFeature,
+      mouseout: resetHighlight
+  });
+}
+
+/**
+ * Haetaan GeoJSON-layerin tyyli
+ * @returns Tyylit-objekti
+ */
+function style() {
+  return {
+      fillColor: 'grey',
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      // dashArray: '3',
+      fillOpacity: 0.7
+  };
+}
+
 
 const MapComponent = () => {
   const [mapData, setMapData] = useState(null)
@@ -19,9 +77,18 @@ const MapComponent = () => {
             attribution: '© OpenStreetMap contributors',
           }).addTo(map)
           mapRef.current = map
+          // Lisätään GeoJSON-layeri
+          L.geoJSON(countries).addTo(map);
+
+          geojson = L.geoJson(countries, {
+            style: style,
+            onEachFeature: onEachFeature
+          }).addTo(map);
         }
       }
     })
+
+
 
     return () => {
       if (mapRef.current !== null) {
